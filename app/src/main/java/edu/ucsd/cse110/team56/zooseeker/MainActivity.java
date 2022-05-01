38323ae34199b0b75467fe90e939b96b12744baf
JingyuanHe1222 <j7he@ucsd.edu>
2022-05-01 11:14:22 -0700

    commit -m "display list"

--- app/src/main/java/edu/ucsd/cse110/team56/zooseeker/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/team56/zooseeker/MainActivity.java
@@ -92,7 +92,7 @@ public class MainActivity extends AppCompatActivity {
         searchView.setOnCloseListener(new SearchView.OnCloseListener() {
             @Override
             public boolean onClose() {
-                showSearchListView();
+                hideSearchListView();
                 return false;
             }
         });
