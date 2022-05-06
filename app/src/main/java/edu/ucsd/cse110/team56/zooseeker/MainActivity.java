cdd5fc4af1fb0cbb2c1626f1725572769c88ffe6
Miranda Li <89241434+unicornholicMiranda@users.noreply.github.com>
2022-05-05 19:33:00 -0700

    Changed background color

--- app/src/main/java/edu/ucsd/cse110/team56/zooseeker/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/team56/zooseeker/MainActivity.java
@@ -6,14 +6,6 @@ import static java.lang.String.valueOf;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
-<<<<<<< HEAD
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.stream.Collectors;
-=======
->>>>>>> aa12311 (commit -m "added animal count display")
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
