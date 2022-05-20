66ce1ddc3f36e719ca28bf0960e2acc811d63977
Qingyang Hu <12138874+hqy2000@users.noreply.github.com>
2022-05-19 22:53:39 -0700

    Fix search results

--- app/src/main/java/edu/ucsd/cse110/team56/zooseeker/activity/adapter/NodeInfoAdapter.java
+++ app/src/main/java/edu/ucsd/cse110/team56/zooseeker/activity/adapter/NodeInfoAdapter.java
@@ -10,13 +10,16 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatCheckedTextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import edu.ucsd.cse110.team56.zooseeker.dao.entity.NodeInfo;
 
 public class NodeInfoAdapter extends ArrayAdapter<NodeInfo> {
+    List<NodeInfo> objects;
     public NodeInfoAdapter(@NonNull Context context, int resource, @NonNull List<NodeInfo> objects) {
         super(context, resource, objects);
+        this.objects = objects;
     }
 
     @NonNull
@@ -27,4 +30,54 @@ public class NodeInfoAdapter extends ArrayAdapter<NodeInfo> {
         return view;
     }
 
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                FilterResults results = new FilterResults();
+                String prefix = constraint.toString().toLowerCase();
+
+                if (prefix.length() == 0)
+                {
+                    ArrayList<NodeInfo> list = new ArrayList<>(objects);
+                    results.values = list;
+                    results.count = list.size();
+                }
+                else
+                {
+                    final ArrayList<NodeInfo> list = new ArrayList<>();
+
+                    for(NodeInfo node: objects) {
+                        if (node.name.toLowerCase().contains(prefix)) {
+                            list.add(node);
+                        } else {
+                            for (String tag: node.tags) {
+                                if (tag.toLowerCase().contains(prefix)) {
+                                    list.add(node);
+                                    break;
+                                }
+                            }
+                        }
+                    }
+
+                    results.values = list;
+                    results.count = list.size();
+                }
+                return results;
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+                List<NodeInfo> nodeList = (ArrayList<NodeInfo>)results.values;
+
+                clear();
+                for (NodeInfo node: nodeList)
+                {
+                    add(node);
+                }
+            }
+
+        };
+    }
 }
