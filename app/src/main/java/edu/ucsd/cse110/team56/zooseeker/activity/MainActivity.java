017cf4363ae6a4150e5b0b6f2972d988de30384b
Qingyang Hu <12138874+hqy2000@users.noreply.github.com>
2022-05-19 23:26:43 -0700

    Save selected exhibits

--- app/src/main/java/edu/ucsd/cse110/team56/zooseeker/activity/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/team56/zooseeker/activity/MainActivity.java
@@ -120,7 +120,7 @@ public class MainActivity extends AppCompatActivity {
     // -------- Retrieve data from database --------
 
     private List<NodeInfo> getAllNodes() {
-        return ZooDatabase.getSingleton(this).zooDao().getAllNodes().stream().filter(n -> n.kind.equals("exhibit")).collect(Collectors.toList());
+        return ZooDatabase.getSingleton(this).zooDao().getNodesWithKind(NodeInfo.Kind.EXHIBIT);
     }
 
     private List<EdgeInfo> getAllEdges() {
