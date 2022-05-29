977a2130f807848ef4a02760ff022dcf138a2bfd
Vince14Genius <vince14genius@icloud.com>
2022-05-28 22:08:00 -0700

    save user preference to local storage

--- app/src/main/java/edu/ucsd/cse110/team56/zooseeker/activity/SettingsActivity.java
+++ app/src/main/java/edu/ucsd/cse110/team56/zooseeker/activity/SettingsActivity.java
@@ -1,8 +1,11 @@
 package edu.ucsd.cse110.team56.zooseeker.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 import edu.ucsd.cse110.team56.zooseeker.R;
 
@@ -12,5 +15,19 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        final var sharedPref = getSharedPreferences(getString(R.string.preference_file_key), MODE_PRIVATE);
+
+        final var styleSwitch = (SwitchCompat) findViewById(R.id.directions_style_switch);
+        styleSwitch.setChecked(sharedPref.getBoolean(getString(R.string.directions_style_is_brief), false));
+
+        styleSwitch.setOnCheckedChangeListener(this::onDirectionsStyleSwitchToggle);
+    }
+
+    private void onDirectionsStyleSwitchToggle(CompoundButton view, boolean isChecked) {
+        final var editor = getSharedPreferences(getString(R.string.preference_file_key), MODE_PRIVATE)
+                .edit();
+        editor.putBoolean(getString(R.string.directions_style_is_brief), isChecked);
+        editor.apply();
     }
 }
