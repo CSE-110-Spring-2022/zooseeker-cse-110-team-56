636334dd831acff560ebd1dcaceaab8b3356086e
Vince14Genius <vince14genius@icloud.com>
2022-05-31 19:44:44 -0700

    added planTest and nextTest

--- app/src/test/java/edu/ucsd/cse110/team56/zooseeker/ExhibitsManagerTest.java
+++ app/src/test/java/edu/ucsd/cse110/team56/zooseeker/ExhibitsManagerTest.java
@@ -11,6 +11,7 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import edu.ucsd.cse110.team56.zooseeker.activity.manager.ExhibitsManager;
 import edu.ucsd.cse110.team56.zooseeker.dao.ZooDatabase;
@@ -85,5 +86,55 @@ public class ExhibitsManagerTest {
 
         // after add
         assert(manager.canSkip());
+    }
+
+    @Test
+    public void planTest() {
+        final var context = ApplicationProvider.getApplicationContext();
+        final var manager = ExhibitsManager.getSingleton(context);
+
+        // add
+        final var nodeInfo0 = manager.getAllExhibits().get(0);
+        manager.add(nodeInfo0);
+        final var nodeInfo1 = manager.getAllExhibits().get(1);
+        manager.add(nodeInfo1);
+        final var nodeInfo2 = manager.getAllExhibits().get(2);
+        manager.add(nodeInfo2);
+
+        // plan
+        manager.plan(nodeInfo1);
+        final var planList = manager.getPlan().stream()
+                .map(pair -> pair.first)
+                .collect(Collectors.toList());
+
+        // check
+        assert(planList.contains(nodeInfo0));
+        assert(planList.contains(nodeInfo1));
+        assert(planList.contains(nodeInfo2));
+        assert(planList.contains(manager.getGate()));
+    }
+
+    @Test
+    public void nextTest() {
+
+        final var context = ApplicationProvider.getApplicationContext();
+        final var manager = ExhibitsManager.getSingleton(context);
+
+        // add
+        final var nodeInfo0 = manager.getAllExhibits().get(0);
+        manager.add(nodeInfo0);
+        final var nodeInfo1 = manager.getAllExhibits().get(1);
+        manager.add(nodeInfo1);
+
+        // plan
+        manager.plan(nodeInfo0);
+        manager.next();
+        final var planList = manager.getPlan().stream()
+                .map(pair -> pair.first)
+                .collect(Collectors.toList());
+
+        // check
+        assert(manager.getVisitedList().contains(nodeInfo0));
+        assert(!manager.getVisitedList().contains(nodeInfo1));
     }
 }
